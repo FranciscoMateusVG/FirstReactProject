72d05d65d4aec4ff9ace669d19bf1e5565316522
Xerox <franciscomateusvg@gmail.com>
2020-03-19 23:33:18 -0400

    Prettier

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,23 +5,24 @@ import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hof/Aux/Auxiliar';
 
-const SideDrawer = (props) => {
-  let attachedClasses = [classes.SideDrawer, classes.Close]
+const SideDrawer = props => {
+	let attachedClasses = [classes.SideDrawer, classes.Close];
 
-  if (props.open) {
-    attachedClasses = [classes.SideDrawer,classes.Open]    
-  }
-  
-  return (
-    <Aux>
-      <Backdrop show={props.open} clicked={props.closed} />
-      <div className={attachedClasses.join(' ')}>
-        <Logo height="11%" />
-        <nav>
-          <Navigationitens />
-        </nav>
-      </div>
-    </Aux>)
+	if (props.open) {
+		attachedClasses = [classes.SideDrawer, classes.Open];
+	}
+
+	return (
+		<Aux>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div className={attachedClasses.join(' ')}>
+				<Logo height='11%' />
+				<nav>
+					<Navigationitens />
+				</nav>
+			</div>
+		</Aux>
+	);
 };
 
 export default SideDrawer;
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import classes from './Input.module.css'
+import React from 'react';
+import classes from './Input.module.css';
 
-const input = (props) => {
-    let inputElement = null;
+const input = props => {
+	let inputElement = null;
 
-    switch (props.inputtype) {
-        case ('select'):
-            let options = props.options.map((element,index) => {
-              return  <option key={index}>{element}</option>                
-            });
-            inputElement = <select className={classes.InputElement} {...props} >
-                            {options}
-                           </select>
-            break;
-        case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />
-            break;
-        default:
-        inputElement = <input className={classes.InputElement} {...props} />    
-    }
+	switch (props.inputtype) {
+		case 'select':
+			let options = props.options.map((element, index) => {
+				return <option key={index}>{element}</option>;
+			});
+			inputElement = (
+				<select className={classes.InputElement} {...props}>
+					{options}
+				</select>
+			);
+			break;
+		case 'textarea':
+			inputElement = <textarea className={classes.InputElement} {...props} />;
+			break;
+		default:
+			inputElement = <input className={classes.InputElement} {...props} />;
+	}
 
-    return (
-        <div className={classes.Input} >
-            <label className={classes.Label}>{props.label}</label>
-            {inputElement}
-        </div>
-    )
- 
-}
+	return (
+		<div className={classes.Input}>
+			<label className={classes.Label}>{props.label}</label>
+			{inputElement}
+		</div>
+	);
+};
 
-export default input
+export default input;
